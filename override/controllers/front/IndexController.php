5ca6c5f5cd7f31cae7a2c9443b3813e8ceada199
satanicman <khai603@gmail.com>
2016-11-24 14:19:00 +0400

    Index css

--- override/controllers/front/IndexController.php
+++ override/controllers/front/IndexController.php
@@ -24,23 +24,11 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class IndexControllerCore extends FrontController
+class IndexController extends IndexControllerCore
 {
-    public $php_self = 'index';
-
-    /**
-     * Assign template vars related to page content
-     * @see FrontController::initContent()
-     */
     public function initContent()
     {
         parent::initContent();
-        $this->addJS(_THEME_JS_DIR_.'index.js');
-
-        $this->context->smarty->assign(array('HOOK_HOME' => Hook::exec('displayHome'),
-            'HOOK_HOME_TAB' => Hook::exec('displayHomeTab'),
-            'HOOK_HOME_TAB_CONTENT' => Hook::exec('displayHomeTabContent')
-        ));
-        $this->setTemplate(_PS_THEME_DIR_.'index.tpl');
+        $this->addCSS(_THEME_CSS_DIR_.'index.css');
     }
 }
